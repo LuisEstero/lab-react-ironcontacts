c46ca466ab4ed1c77601f26419c4767da907826b
LuisEstero <luisestero@gmail.com>
2022-07-12 21:06:33 +0200

    Colapso Total

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 // src/App.js
+import contacts from "./contacts.json";
+
 import "./App.css";
+
+
+
+
+
 
 function App() {
   return <div className="App"></div>;
 }
+
+
+
+
+
+
 export default App;
